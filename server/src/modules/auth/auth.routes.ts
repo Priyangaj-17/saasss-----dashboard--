80e0ef78f796b9bc75cf235f2a8685197da79f82
Priyangaj-17 <priyangaj17@gmai.com>
2026-02-12 16:35:04 +0530

    server-changes

--- server/src/modules/auth/auth.routes.ts
+++ server/src/modules/auth/auth.routes.ts
@@ -1,29 +1,9 @@
 import { Router } from "express";
-import User from "./auth.model";
+import { signup, login } from "./auth.controller";
 
 const router = Router();
 
-router.post("/signup", async (req, res) => {
-  const { name, email, password } = req.body;
-
-  // 1. Check if user exists
-  const existingUser = await User.findOne({ email });
-  if (existingUser) {
-    return res.status(400).json({ message: "User already exists" });
-  }
-
-  // 2. Create user
-  const user = await User.create({
-    name,
-    email,
-    password, // hashing later
-  });
-
-  // 3. Send response
-  res.status(201).json({
-    message: "User created",
-    userId: user._id,
-  });
-});
+router.post("/signup", signup);
+router.post("/login", login);
 
 export default router;
